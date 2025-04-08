287f4a717b36a893c77a65e0cc420dff92506f57
JamesLove03 <jameslove2k22@gmail.com>
2025-04-08 17:19:18 -0400

    tested and is working chash.c

--- README.md
+++ README.md
@@ -1,1 +1,35 @@
 # PA-2-Concurrent-Hash-Table
+
+
+
+James Love Prompts Used:
+
+"Can you give me a way to read input in the folllowing format into these 3 categories seperated by commas 
+insert,Richard Garriot,40000
+insert,Sid Meier,50000
+insert,Shigeru Miyamoto,51000
+delete,Sid Meier,0
+insert,Hideo Kojima,45000
+insert,Gabe Newell,49000
+insert,Roberta Williams,45900
+delete,Richard Garriot,0
+insert,Carol Shaw,41000
+search,Sid Meier,0"
+
+"the first line of input gives the size of the input like this: only the number in the middle needs to be considered
+threads,10,0"
+
+"how to compile and run a main.c file using a make file"
+
+"I need to read a similar input as this in line by line in a c program. I need code that will take each individual line assign 4 different variables as separated by commas then pass the results to a function Sorter() that takes the 4 variables as input. I have already handled the first line using an fgets function
+threads,10,0
+insert,Richard Garriot,40000
+insert,Sid Meier,50000
+insert,Shigeru Miyamoto,51000
+delete,Sid Meier,0
+insert,Hideo Kojima,45000
+insert,Gabe Newell,49000
+insert,Roberta Williams,45900
+delete,Richard Garriot,0
+insert,Carol Shaw,41000
+search,Sid Meier,0"
--- chash.c
+++ chash.c
@@ -1,28 +1,80 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "src/hash.c"
+#include "src/rwlock.c"
+
+#define MAX_LINE 256
+
+void Sorter(const char *operation, const char *name, int value) {
+    printf("Sorter() called with: [%s], [%s], [%d]\n",
+           operation, name, value);
+}
+
+
+
 
 int main() {
     
-    FILE *file;
-    file = fopen("commands.txt", "r"); //opens file
+    FILE *inpfile, *outputfile;
+    inpfile = fopen("commands.txt", "r"); //opens file
+    outputfile = fopen("output.txt", "w");
     
     int num_threads = 0; //holds number of threads
-    char buffer[30]; //holds first line
+    char buffer[MAX_LINE]; //holds first line
     int dummy = 0;
     char dummys[2];
+    int line_num = 0;
 
-    if (file == NULL )
+    if ((inpfile == NULL) || (outputfile == NULL))
     {
-        perror("Error opeNing file");
+        perror("Error opening file(s)");
         return 1;
     }
 
-    if(fgets(buffer, sizeof(buffer), file) != NULL) {
+
+    if(fgets(buffer, sizeof(buffer), inpfile) != NULL) {
 
         if(sscanf(buffer,"%49[^,],%d,%d", dummys, &num_threads, &dummy))
-            printf("Running %d threads", num_threads);
+            fprintf(outputfile, "Running %d threads", num_threads);
         
     }
 
 
+    buffer[strcspn(buffer, "\n")] = 0;
+    line_num++;
+
+
+    while (fgets(buffer, sizeof(buffer), inpfile) != NULL) {
+        char *op = NULL, *name = NULL;
+        char *token;
+        int value = 0;
+
+        // Strip newline
+        buffer[strcspn(buffer, "\n")] = 0;
+        line_num++;
+
+        // Tokenize the line
+        token = strtok(buffer, ",");
+        if (token) op = token;
+
+        token = strtok(NULL, ",");
+        if (token) name = token;
+
+        token = strtok(NULL, ",");
+        if (token) value = atoi(token);
+
+        if (op && name) {
+            Sorter(op, name, value); // Call your function
+        } else {
+            printf("Invalid line format at line %d.\n", line_num + 1);
+        }
+    }
+    
+
+
     return 0;
 }
+
+
+
